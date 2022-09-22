657268a7a5d1cb02243c79cae4f80aed315ae8b7
Marcos-Goncalves <kirottow17@gmail.com>
2022-09-21 23:33:22 -0300

    feat: implementando funcionalidade do botão compartilhar

--- app/src/main/java/com/app/bissextoactivity/DescobrirActivity.java
+++ app/src/main/java/com/app/bissextoactivity/DescobrirActivity.java
@@ -2,7 +2,9 @@ package com.app.bissextoactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,13 +24,30 @@ public class DescobrirActivity extends AppCompatActivity {
 
         ((TextView)findViewById(R.id.idNome)).setText(nome);
         ((TextView)findViewById(R.id.idAno)).setText(String.valueOf(ano));
+
+        bissexto();
     }
 
-    public void bissexto(){
+    public boolean bissexto(){
         if(ano % 100 == 0 && ano % 400 == 0 || ano % 100 != 0 && ano % 4 == 0){
             ((TextView)findViewById(R.id.idResultado)).setText(ano + " é bissexto!");
+            return true;
         }else {
             ((TextView)findViewById(R.id.idResultado)).setText(ano + " não é bissexto!");
+            return false;
         }
+    }
+
+    public void compartilhar(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_SUBJECT, ("Ano bissexto!"));
+        if(bissexto() == true) {
+            intent.putExtra(Intent.EXTRA_TEXT, (nome + ", o ano " + ano + " é um ano bissexto!"));
+        } else {
+            intent.putExtra(Intent.EXTRA_TEXT, (nome + ", o ano " + ano + " não é um ano bissexto!"));
+        }
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, "Compartilhando com ..."));
     }
 }
